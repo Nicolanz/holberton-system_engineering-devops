7e7008e1b624351ce6f135c1e0352ad8b5d507e3
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-08-03 23:28:49 +0000

    Task #0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,1 +1,10 @@
-#!/usr/bin/env bash
+# Creates a file with puppet
+node default {
+file { '/tmp/holberton':
+ensure  => 'present',
+mode    => '0744',
+group   => 'www-data',
+owner   => 'www-data',
+content => 'I love Puppet',
+}
+}
