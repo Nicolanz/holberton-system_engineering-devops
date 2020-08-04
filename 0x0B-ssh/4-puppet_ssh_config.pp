8d9a378b6e1138cf418d6a9dcb2b1d1965af0ada
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-08-04 21:27:37 +0000

    Task #4

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,1 +1,8 @@
-#!/usr/bin/env bash
+# Adds some content
+node default {
+file { '/etc/ssh/ssh_config':
+ensure  => 'present',
+content => '   IdentityFile ~/.ssh/holberton
+   PasswordAuthentication no'
+}
+}
