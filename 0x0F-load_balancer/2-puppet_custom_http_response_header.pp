77e93bd3e821933af15fc6e0d78e3f24b5d8ddaa
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-08-15 03:52:01 +0000

    Task 2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,5 +1,5 @@
-# kill a process
-exec { 'Configure a server':
+# Configure a server
+exec { 'configureItKnow':
 command => "sed -i 's/server_name _;/add_header X-Served-By $HOSTNAME;/' /etc/nginx/sites-enabled/default;",
 path    => ['/usr/bin', '/usr/sbin',],
 }
