6867006ef707154cb7f1862282093245ccabde7d
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-09-30 23:52:33 +0000

    Task 0 and rm *~

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 #fix It
 exec { 'debugging':
-    command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+    command => 'sed -i s/phpp/php/ /var/www/html/wp-settings.php',
     path    => '/usr/bin:/sbin:/bin:/usr/sbin'
 }
